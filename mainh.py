8f846957695224c5ce399a079d4a0e226464feff
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-10-07 13:37:18 +0530

    Update mainh.py

--- mainh.py
+++ mainh.py
@@ -81,7 +81,7 @@ while True:
         elif 'truck' in c:
              list2.append([x1,y1,x2,y2])
              truck.append(c)
-######################CAR######################################     
+
     bbox_idx=tracker.update(list)
     for bbox in bbox_idx:
         x3,y3,x4,y4,id1=bbox
